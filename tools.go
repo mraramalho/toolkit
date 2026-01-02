45505e1a13b3da3a4362e85b60091f14e3ae4390
Andre Luis Azevedo Ramalho - PrestServ <andreramalho.prestserv+petro@petrobras.com.br>
2026-01-02 15:09:41 -0300

    doc: fixed typo

--- tools.go
+++ tools.go
@@ -29,7 +29,7 @@ type ErrorTemplate interface {
 }
 
 // Tools is the type used to instantiate this module. Any variable of this type will
-// have access to all the methods with the reciever *Tools
+// have access to all the methods with the receiver *Tools
 type Tools struct {
 	MaxFileSize           int
 	AllowedFileTypes      []string
